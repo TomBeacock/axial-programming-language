8228038dc647a0fe26cdcc89fd30253c526f4110
Thomas Beacock <thomasbeacock@gmail.com>
2022-12-04 23:39:45 +0000

    Add arithmetic optimization to AST

--- app/Parsers/Expr.hs
+++ app/Parsers/Expr.hs
@@ -34,9 +34,9 @@ arithmetic = arithmetic' id
         arithmetic' f = do
             x <- term
             do
-                symbol "+"; arithmetic' (Binary Add (f x))
-                <|> do symbol "-"; arithmetic' (Binary Sub (f x))
-                <|> return (f x)
+                symbol "+"; arithmetic' (optimize . Binary Add (f x))
+                <|> do symbol "-"; arithmetic' (optimize . Binary Sub (f x))
+                <|> return (optimize (f x))
 
 term :: Parser Expr
 term = term' id
@@ -44,17 +44,33 @@ term = term' id
         term' f = do
             x <- factor
             do
-                symbol "*"; term' (Binary Mul (f x))
-                <|> do symbol "/"; term' (Binary Div (f x))
-                <|> do symbol "%"; term' (Binary Mod (f x))
-                <|> return (f x)
+                symbol "*"; term' (optimize . Binary Mul (f x))
+                <|> do symbol "/"; term' (optimize . Binary Div (f x))
+                <|> do symbol "%"; term' (optimize . Binary Mod (f x))
+                <|> return (optimize (f x))
 
 factor :: Parser Expr
 factor = do
     symbol "+"; Unary Pos <$> factor
-    <|> do symbol "-"; Unary Neg <$> factor
-    <|> do symbol "("; x <- expr; symbol ")"; return x
+    <|> do symbol "-"; x <- Unary Neg <$> factor; return (optimize x)
+    <|> do symbol "("; x <- expr; symbol ")"; return (optimize x)
     <|> Literal <$> intLiteral
+
+optimize :: Expr -> Expr
+optimize (Unary op (Literal a)) = let f = unaryFunctionOf op in Literal $ f a
+optimize (Binary op (Literal a) (Literal b)) = let f = binaryFunctionOf op in Literal $ f a b
+optimize expr = expr
+
+unaryFunctionOf :: Num a => UnaryOperator -> (a -> a)
+unaryFunctionOf Pos = id
+unaryFunctionOf Neg = \a -> -a
+
+binaryFunctionOf :: Integral a => BinaryOperator -> (a -> a -> a)
+binaryFunctionOf Add = (+)
+binaryFunctionOf Sub = (-)
+binaryFunctionOf Mul = (*)
+binaryFunctionOf Div = div
+binaryFunctionOf Mod = mod
 
 -- Literal parsers
 intLiteral :: Parser Int
